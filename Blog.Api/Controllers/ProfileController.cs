378f932cc1e8e15937452395b9968021c096ad25
mare32 <marechl32@gmail.com>
2022-06-19 10:03:00 +0200

    added /profile endpoint which shows the logged user info

---
+++ Blog.Api/Controllers/ProfileController.cs
@@ -0,0 +1,32 @@
+﻿using Blog.Application.UseCases.DTO;
+using Blog.Application.UseCases.Queries;
+using Blog.Domain;
+using Blog.Implementation;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Blog.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class ProfileController : ControllerBase
+    {
+        // /profile da prikaze sve podatke o ulogovanom korisniku
+        private IApplicationUser _user;
+        private UseCaseHandler _handler;
+        public ProfileController(IApplicationUser user, UseCaseHandler handler)
+        {
+            _user = user;
+            _handler = handler;
+        }
+
+        [HttpGet]
+        public IActionResult Get([FromServices]IApplicationUser user,[FromServices]IGetOneUserQuery query)
+        {
+            var id = user.Id;
+            return Ok(_handler.HandleQuery(query, id));
+        }
+    }
+}
